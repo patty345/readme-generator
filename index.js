75d09147ea17dc58aa99d163b7e918ea0545199e
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-30 12:31:21 -0600

    added dependencies and array of questions

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-// TODO: Include packages needed for this application
+// Dependecies 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions to be asked 
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GPL3.0", "BSD2", "BSD3", "None"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
